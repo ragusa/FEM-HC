3e0c84a1c17962a948937238bfbda70d7d532dd9
jianigao <jiani.gao@icloud.com>
2016-04-26 16:06:31 -0500

    code with problems

--- hcg.m
+++ hcg.m
@@ -3,13 +3,16 @@ function F=driver
 % clear the console screen
 clc; close all;clf
 % load the data structure with info pertaining to the physical problem
+global dat
 dat.condu=@condu;
 dat.esrc=@esrc;
-dat.width=[0.003175 0.034823 0.034829];
-bc.left.type=2; %0=neumann, 1=robin, 2=dirichlet
-bc.left.C=5; % (that data is C in: +Ddu/dn=C // u/4+D/2du/dn=C // u=C)
+dat.hgap=15764;
+dat.hcv=20000;
+dat.width=[0.003175 0.034823 0.036];
+bc.left.type=1; %0=neumann, 1=robin, 2=dirichlet
+bc.left.C=400; % (that data is C in: kdu/dn=C // u-k/hcv*du/dn =C // u=C)
 bc.rite.type=1;
-bc.rite.C=2;
+bc.rite.C=80;
 dat.bc=bc; clear bc;
 
 % load the numerical parameters, npar, structure pertaining to numerics
@@ -54,14 +57,14 @@ xlabel('Width')
 ylabel('Temperature')
 
 % verification is always good
-verif_hc_eq(dat)
+verif_hc_eq;
 
 return
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function u=solve_fem3(dat,npar)
+function u=solve_fem3(dat, npar)
 
 % initial guess
 % (not needed if a direct method is used to solve the linear system)
@@ -78,6 +81,7 @@ end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 function [A,rhs]=assemble_system(npar,dat)
+
 % assemble the matrix, the rhs, apply BC
 
 % shortcuts
@@ -110,7 +114,7 @@ f=zeros(porder+1,1);
 
 % definition of the weak form:
 % int_domain (grad u D grad b) - int_bd_domain (b D grad u n) ...
-%      + int_domain( XSa u b) = int_domain (b rhs)
+%      = int_domain (b rhs)
 
 % loop over elements
 for iel=1:npar.nel
@@ -137,6 +141,11 @@ for iel=1:npar.nel
     rhs(gn(iel,:)) = rhs(gn(iel,:)) + f*Jac;
 end
 
+A(g1,g1)=A(g1,g1)+dat.hgap;
+A(g1,g2)=A(g1,g2)-dat.hgap;
+A(g2,g1)=A(g2,g1)-dat.hgap;
+A(g1,g2)=A(g1,g2)+dat.hgap;
+
 % apply natural BC
 Dirichlet_nodes=[];
 Dirichlet_val=[];
@@ -144,38 +153,18 @@ switch dat.bc.left.type
     case 0 % Neumann, int_bd_domain (b D grad u n) is on the RHS
         rhs(1)=rhs(1)+dat.bc.left.C;
     case 1 % Robin
-        A(1,1)=A(1,1)+1/2;
-        rhs(1)=rhs(1)+2*dat.bc.left.C;
+        A(1,1)=A(1,1)+dat.hcv;
+        rhs(1)=rhs(1)+dat.hcv*dat.bc.left.C;
     case 2 % Dirichlet
         Dirichlet_nodes=[Dirichlet_nodes 1];
         Dirichlet_val=[Dirichlet_val dat.bc.left.C];
 end
 switch dat.bc.rite.type
     case 0 % Neumann, int_bd_domain (b D grad u n) is on the RHS
-        rhs(g1)=rhs(g1)+dat.bc.rite.C;
-    case 1 % Robin
-        A(g1,g1)=A(g1,g1)+1/2;
-        rhs(g1)=rhs(g1)+2*dat.bc.rite.C;
-    case 2 % Dirichlet
-        Dirichlet_nodes=[Dirichlet_nodes g1];
-        Dirichlet_val=[Dirichlet_val dat.bc.rite.C];
-end
-switch dat.bc.left.type
-    case 0 % Neumann, int_bd_domain (b D grad u n) is on the RHS
-        rhs(g2)=rhs(g2)+dat.bc.left.C;
-    case 1 % Robin
-        A(g2,g2)=A(g2,g2)+1/2;
-        rhs(g2)=rhs(g2)+2*dat.bc.left.C;
-    case 2 % Dirichlet
-        Dirichlet_nodes=[Dirichlet_nodes g2];
-        Dirichlet_val=[Dirichlet_val dat.bc.left.C];
-end
-switch dat.bc.rite.type
-    case 0 % Neumann, int_bd_domain (b D grad u n) is on the RHS
         rhs(n)=rhs(n)+dat.bc.rite.C;
     case 1 % Robin
-        A(n,n)=A(n,n)+1/2;
-        rhs(n)=rhs(n)+2*dat.bc.rite.C;
+        A(n,n)=A(n,n)+dat.hcv;
+        rhs(n)=rhs(n)+dat.hcv*dat.bc.rite.C;
     case 2 % Dirichlet
         Dirichlet_nodes=[Dirichlet_nodes n];
         Dirichlet_val=[Dirichlet_val dat.bc.rite.C];
@@ -268,49 +257,81 @@ return
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function verif_hc_eq(dat)
+function verif_hc_eq
+global dat
 
-cd=dat.condu; src=dat.esrc; L=dat.width;
+cd=dat.condu; src=dat.esrc; hgap=dat.hgap; hcv=dat.hcv; L=dat.width; 
+
+% general form of the solution:
+% Zone 1 : T1 = B1*x + E1
+% dT1/dx= B1
+% Zone 2 : T2 = -q/(2*k2)*(x.^2) + B2*x + E2 = Y2*(x.^2) + B2*x + E2
+% dT2/dx= -q/k2*x + B2 = Y1*x + B2
+% Zone 3 : T3 = B3*x + E3
+% dT3/dx= B3
+
+% definition of 2 coefficients
+Y1=-src(L(2))/cd(L(1));
+Y2=-src(L(2))/(2*cd(L(2)));
 
 switch dat.bc.left.type
     case 0 % Neumann
-        % +Ddu/dn=C on the left becomes: -Ddu/dx=C <==> -D.B=C <==> B=-C/D
+        % k1*du1/dn=C on the left becomes: -k1*du1/dx=C 
+        % <==> -k1*B1=C <==> B1=-C/k1
         mat(1,1:6) =[1,0,0,0,0,0];
-        b(1) = -dat.bc.left.C / cd(0);
+        b(1) = -dat.bc.left.C / cd(L(1));
     case 1 % Robin
-        % u/4+D/2du/dn=C on the left becomes: u/4-D/2du/dx=C 
-        % <==> -(D/2)*B+E/4=C 
-        mat(1,1:6) =[-cd(0)/2,1/4];
+        % u1-k1/hcv*du1/dn =C on the left becomes: u1+k1/hcv*du1/dx =C
+        % <==> k1/hcv*B1+E1=C
+        mat(1,1:6) =[cd(L(1))/hcv,1,0,0,0,0]; %cd(L(1))=cd(L(0)) in the function definition
         b(1) = dat.bc.left.C;
     case 2 % Dirichlet
+        % u1=C <==> E1=C
         mat(1,1:6) =[0,1,0,0,0,0];
         b(1) = dat.bc.left.C;
 end
 switch dat.bc.rite.type
     case 0 % Neumann
-        % +Ddu/dn=C on the right becomes: Ddu/dx=C <==> D.B=C <==> B=C/D
+        % k3*du3/dn=C on the right becomes: k3*du3/dx=C 
+        % <==> k3*B3=C <==> B3=C/k3
         mat(6,1:6) =[0,0,0,0,1,0];
         b(6) = dat.bc.rite.C / cd(L(3));
     case 1 % Robin
-        % u/4+D/2du/dn=C on the right becomes: u/4+D/2du/dx=C 
-        % <==> (BL+E)/4 + (D/2)*B = C 
-        mat(6,1:6) =[0,0,0,0,L(3)/4+cd(L(3))/2,1/4];
+        % u3-k3/hcv*du3/dn =C on the right becomes: u3-k3/hcv*du3/dx =C
+        % <==> (L3-k3/hcv)B3+E3=C
+        mat(6,1:6) =[0,0,0,0,L(3)-cd(L(3))/hcv,1];
         b(6) = dat.bc.rite.C;
     case 2 % Dirichlet
+        % u3=C <==> B3*L+E3=C
         mat(6,1:6) =[0,0,0,0,L(3),1];
         b(6) = dat.bc.rite.C;
 end
 
-Y=-src(L(1))/(2*cd(L(1)));
+% fixed conditions
+% continuity of T and flux between zone 1 and zone 2 (interface L1)
+% T1(L1)=T2(L1) <==> B1*L1+E1=Y2*(L1^2)+B2*L1+E2
+% <==> B1*L1+E1-B2*L1-E2=Y2*(L1^2)
+mat(2,1:6) =[L(1),1,-L(1),-1,0,0];
+b(2) =Y2*L(1)*L(1);
+% phi1(L1)=phi2(L1) <==> k1*B1=k2*(Y1*L1+B2)
+% <==> (k1/k2)*B1-B2=Y1*L1
+mat(3,1:6) =[cd(L(1))/cd(L(2)),0,-1,0,0,0];
+b(3) =Y1*L(1);
 
-mat(2,1:6) =[1,0,-1,0,0,0];
-mat(3,1:6) =[0,0,cd(L(1)),0,-cd(L(3)),0];
-mat(4,1:6) =[L(1),1,-L(1),-1,0,0];
-mat(5,1:6) =[0,0,L(2),1,-L(2),-1];
-b(2) =2*Y*L(1);
-b(3) =-2*Y*L(2);
-b(4) =Y*L(1)*L(1);
-b(5) =-Y*L(2)*L(2);
+% discontinuity of T between zone 2 and zone 3 (interface L2)
+% T2(L2)=Y2*(L2^2)+B2*L2+E2
+% T3(L2)=B3*L2+E3
+% Tg=(T2(L2)+T3(L2))/2
+% -k2*dT2/dx=hgap(T2(L2)-Tg)
+% <==> -k2(-q/k2*L2+B2)=hgap(T2(L2)-T3(L2))/2
+% <==> (2k2+L2*hgap)B2+hgap*E2-L2*hgap*B3-hgap*E3=-hgap*Y2*(L2^2)+2*q*L2
+mat(4,1:6) =[0,0,2*cd(L(2))+L(2)*hgap,hgap,-L(2)*hgap,-hgap];
+b(4) =-hgap*Y2*L(2)*L(2)+2*src(L(2))*L(2);
+% -k3*dT3/dx=hgap(Tg-T3(L2))
+% <==> -k3*B3=hgap(T2(L2)-T3(L2))/2
+% <==> L2*hgap*B2+hgap*E2+(2k3-L2*hgap)*B3-hgap*E3=-hgap*Y2*(L2^2)
+mat(5,1:6) =[0,0,L(2)*hgap,hgap,2*cd(L(3))-L(2)*hgap,-hgap];
+b(5) =-hgap*Y2*L(2)*L(2);
 
 % get coefficient for the analytical solution
 a=mat\b';
@@ -318,30 +339,34 @@ x1=linspace(0,L(1));
 x2=linspace(L(1),L(2));
 x3=linspace(L(2),L(3));
 y1=a(1)*x1+a(2);
-y2=Y*(x2.^2)+a(3)*x2+a(4);
+y2=Y2*(x2.^2)+a(3)*x2+a(4);
 y3=a(5)*x3+a(6);
-plot(x1,y1,x2,y3,x3,y3,'r-'); hold all;
+
+plot(x1,y1,x2,y2,x3,y3); hold all;
+title('1D heat conduction problem')
+xlabel('Width')
+ylabel('Temperature')
 
 return
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function y=condu(x)
-w=[0.003175 0.034823 0.034829];
-if x<=w(2)
-    y=0.018;
+global dat
+if x<=dat.width(2)
+    y=18;
 else
-    y=0.0162;
+    y=16;
 end
 return
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function y=esrc(x)
-w=[0.003175 0.034823 0.034829];
-if (x<=w(1)) | (x>=w(2)) 
-    y=0;
+global dat
+if (x>dat.width(1) & x<=dat.width(2)) 
+    y=5000000;
 else
-    y=0.5;
+    y=0;
 end
 return
 end
