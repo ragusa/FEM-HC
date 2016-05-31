f72c94e22ed5afb3588014c0e54c2297d23cd138
jianigao <jiani.gao@icloud.com>
2016-05-31 11:10:02 -0500

    update

--- esrct.m
+++ esrct.m
@@ -1,5 +1,6 @@
 function y=esrct(x,t)
-% 1MW, 90 fuel elements, diameter = 3.4..cm, fuel height = 38 cm)
-% 1e6/90/(pi*.034823^2/4*.381)
-y=30.6e6*min(1,t/10);
+% 1MW, 90 fuel elements, Zr inner rod radius=0.003175m
+% fuel meat radius=0.0174115m, fuel height = 0.381m)
+% 1e6/90/(pi*(0.0174115^2-0.003175^2)*.381)
+y=3.1674e7*min(1,t/10);
 end
